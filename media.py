949b36fd929473cf7c46bc656adece80b8dda5be
lokeshkrishnagaddam <38466233+lokeshkrishnagaddam@users.noreply.github.com>
2018-07-23 15:04:57 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,6 +1,6 @@
-class Movies:
-    def __init__(self, titles, storylines, images, youtube):
-        self.titles = titles
-        self.storylines = storylines
-        self.poster_image_urls = images
-        self.trailer_youtube_urls = youtube
+class M:
+    def __init__(self, t, s, i, y):
+        self.t = t
+        self.s = s
+        self.i = i
+        self.y = y
