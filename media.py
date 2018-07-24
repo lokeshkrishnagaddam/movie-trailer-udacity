80bfdf446d3bf021128fccde5ab1485dd1145fa4
lokeshkrishnagaddam <38466233+lokeshkrishnagaddam@users.noreply.github.com>
2018-07-24 08:36:19 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,7 +1,9 @@
 import webbrowser
-class Movies:
-    def __init__(self, t, s, i, y):
-        self.t = t
-        self.s = s
-        self.i = i
-        self.y = y
+
+class Movie():
+    def __init__(self, title, year, poster_url, trailer_url):
+        self.title = title
+        self.year = year
+        self.poster_url = poster_url
+        self.trailer_url = trailer_url
+
