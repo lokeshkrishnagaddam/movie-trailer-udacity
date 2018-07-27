ff26e09dec5fb616878ef3cb85fab62715a5538d
lokeshkrishnagaddam <38466233+lokeshkrishnagaddam@users.noreply.github.com>
2018-07-27 09:59:03 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -11,7 +11,7 @@ trailer_url (url to youtube trailer)
 """
 
 bahubali = media.Movie("BAHUBALI", "king", "https://en.wikipedia.org/wiki/Baahubali:_The_Beginning#/media/File:Baahubali_poster.jpg", "https://www.youtube.com/watch?v=qD-6d8Wo3do")  # NOQA
-merchi = media.Movie("MIRCHI", "About love", "https://en.wikipedia.org/wiki/Mirchi_(film)#/media/File:Mirchi_Poster.jpg", "https://www.youtube.com/watch?v=I6QLMyeC1n8")  # NOQA
+mirchi = media.Movie("MIRCHI", "About love", "https://en.wikipedia.org/wiki/Mirchi_(film)#/media/File:Mirchi_Poster.jpg", "https://www.youtube.com/watch?v=I6QLMyeC1n8")  # NOQA
 temper = media.Movie("TEMPER", "About a missing girl", "http://3.bp.blogspot.com/-aBL2jjTCUeg/VLKbElMbFNI/AAAAAAAAAc4/OwE4Sebauig/s1600/Temper%2BMovie1.jpg", "https://www.youtube.com/watch?v=SQgRN5tu1f4")  # NOQA
 gowthamnanda = media.Movie("GOWTHAM NANDA", "About Money", "https://m.media-amazon.com/images/M/MV5BY2U1ZGQ2MGUtYzhhNC00NTg4LWEyODEtODY4YzNiNGUwMzNiXkEyXkFqcGdeQXVyMzYxOTQ3MDg@._V1_QL50_SY1000_CR0,0,705,1000_AL_.jpg", "https://www.youtube.com/watch?v=ztWcdGn5thU")  # NOQA
 bharatanenenu = media.Movie("BHARAT ANE NENU", "About politics", "https://en.wikipedia.org/wiki/Bharat_Ane_Nenu#/media/File:Bharat_Ane_Nenu_poster.jpg", "https://www.youtube.com/watch?v=IvSoRWkaKn4&t=97s")  # NOQA
