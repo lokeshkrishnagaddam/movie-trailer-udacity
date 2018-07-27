e67d9c148ea7153c995699dbb11dd207b674c451
lokeshkrishnagaddam <38466233+lokeshkrishnagaddam@users.noreply.github.com>
2018-07-27 23:19:45 +0530

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -140,7 +140,7 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title = movie.title,
-            poster_url = movie.poster_url,
+            poster_image_url = movie.poster_url,
             trailer_youtube_id = trailer_youtube_id)
     return content
 
